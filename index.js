f8a5c9f7efb81eab841e29b0260a0aad5ca00413
MdMuzaffar <54287360+MdMuzaffar@users.noreply.github.com>
2023-07-01 23:11:18 +0530

    Clock with hour,min-sec

--- index.js
+++ index.js
@@ -298,3 +298,26 @@ for (let i = 0; i < items.length; i++) {
 
 }
 // console.log(found)
+// debugger
+
+function setDate() {
+    const secondHands = document.querySelector('.secondsHand');
+    const minutesHands = document.querySelector('.minutesHand');
+    const hoursHands = document.querySelector('.hourHand');
+
+    const now = new Date();
+
+    const seconds = now.getSeconds();
+    const secondsDegree = ((seconds / 60) * 360) + 90;
+    secondHands.style.transform = `rotate(${secondsDegree}deg)`;
+
+    const minutes = now.getMinutes();
+    const minutesDegree = ((minutes / 60) * 360) + 90;
+    minutesHands.style.transform = `rotate(${minutesDegree}deg)`;
+
+    const hours = now.getHours();
+    const hoursDegree = ((hours / 12) * 360) + 90;
+    hoursHands.style.transform = `rotate(${hoursDegree}deg)`;
+    // console.log(seconds);
+}
+setInterval(setDate, 1000);
